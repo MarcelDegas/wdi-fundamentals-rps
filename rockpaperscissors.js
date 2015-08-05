3ff7a8da40bb56ab0f4159f5f47b8446f6b6f565
Marcel Degas <marcel.degas@gmail.com>
2015-08-05 11:35:09 -0700

    Additional edits to if... end.. statements

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -37,33 +37,35 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    if ((playerMove === "rock") && (computerMove === "paper") {
+    if ((playerMove === "rock") && (computerMove === "paper")) {
     	winner = "computer";
     }
-    else if ((playerMove === "rock") && (computerMove === "rock") {
+    else if ((playerMove === "rock") && (computerMove === "rock")) {
     	winner = "tie";
     }
-    else if ((playerMove === "rock") && (computerMove === "scissors") {
+    else if ((playerMove === "rock") && (computerMove === "scissors")) {
     	winner = "player";
     }
-     else if ((playerMove === "scissors") && (computerMove === "scissors") {
+     else if ((playerMove === "scissors") && (computerMove === "scissors")) {
     	winner = "tie";
     }
-      else if ((playerMove === "scissors") && (computerMove === "paper") {
+      else if ((playerMove === "scissors") && (computerMove === "paper")) {
     	winner = "player";
     }
-      else if ((playerMove === "scissors") && (computerMove === "rock") {
+      else if ((playerMove === "scissors") && (computerMove === "rock")) {
     	winner = "computer";
     }
-      else if ((playerMove === "paper") && (computerMove === "scissors") {
+      else if ((playerMove === "paper") && (computerMove === "scissors")) {
     	winner = "computer";
     }
-      else if ((playerMove === "paper") && (computerMove === "paper") {
+      else if ((playerMove === "paper") && (computerMove === "paper")) {
     	winner = "tie";
     }
-      else if ((playerMove === "paper") && (computerMove === "rock") {
+      else if ((playerMove === "paper") && (computerMove === "rock")) {
     	winner = "player";
     }
+    console.log(winner);
+}
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
